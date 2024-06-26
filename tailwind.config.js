ecacb1d50b67c186c943ff16c0d8e3b3a618805f
Mintaek_CHO <mintaek5555@gmail.com>
2024-06-26 22:42:05 +0900

    init: react-beautiful-dnd, react-router-dom, react-transition-group - Project setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     mode: 'jit',
     theme: {
-        extend: {},
+        extend: {
+            extend: {
+                keyframes: {
+                    'input-focus': {
+                        '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+                        '50%': { transform: 'scale(1.1)', opacity: '0.7' },
+                    },
+                },
+                animation: {
+                    'input-focus': 'input-focus 0.5s ease-in-out',
+                },
+            },
+        },
     },
     plugins: [],
 };
